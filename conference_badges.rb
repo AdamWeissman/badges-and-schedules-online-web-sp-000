1f3c731eddd5b23f3779b500e0f22f81ec195419
Adam Weissman <adam@comedytragedyepic.com>
2019-09-13 09:06:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts attendees.collect {|attendee| puts "#{batch_badge_creator(attendee)} #{assign_rooms(attendee)}"} 
+  puts attendees.collect {|attendee| "#{batch_badge_creator(attendee)} #{assign_rooms(attendee)}"} 
 end
