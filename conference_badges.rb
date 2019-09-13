cc38288e64d67736586ac1b35ea6db3240f66751
Adam Weissman <adam@comedytragedyepic.com>
2019-09-13 08:43:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(attendees)
   attendees.collect {|name| badge_maker(name)}
 end
 
-def assign_room(attendees)
+def assign_rooms(attendees)
   p_w_r = [] #person_with_room
   until p_w_r.length == attendees.length
     attendees.each_with_index do |name, room|
