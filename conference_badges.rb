d3bfa69cfa5533f565e22bee12d665cce8dc809c
Adam Weissman <adam@comedytragedyepic.com>
2019-09-13 08:36:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |name|
-    badges << "Hello, my name is #{name}."
-  end
-  return badges
+  attendees.collect {|name| badge_maker(name)}
 end
 
